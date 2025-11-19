e118d60ceacda8cd8c674459b7593a85306cd6af
Dimitris Terzidis <dterzidis2008@gmail.com>
2025-11-19 21:25:43 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://dimitristerzz.github.io/gitendar",
+  server: {
+    host: true,
+    port: 8080
+  }
+});
